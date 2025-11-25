19e31467d60dd7754a00bfb3b40b90387b568930
Daniel092025 <danbo092025@gmail.com>
2025-11-25 11:58:48 +0100

    Startet oppsett av droner

--- Simulations/ThreadRace.cs
+++ Simulations/ThreadRace.cs
@@ -9,5 +9,24 @@ public class ThreadRace
         Console.WriteLine("=== Thread Race === \n");
 
         var drone1 = new DroneModel("Alpha", 5, 500);
+        var drone2 = new DroneModel("Beta" , 5, 300);
+
+       Thread thread1 = new Thread(() => FlyDrone (drone1));
+       Thread thread2 = new Thread(() => FlyDrone (drone2));
+
+       Console.WriteLine("Starter drone flyving");
+
+       thread1.Start();
+       thread2.Start();
+
+       thread1.Join();
+       thread2.Join();
+
+       Console.WriteLine("Alle dronene har landet");
+    }
+
+    private static void FlyDrone(DroneModel drone)
+    {
+        Console.WriteLine($"{drone.Name} tar av!");
     }
 }
