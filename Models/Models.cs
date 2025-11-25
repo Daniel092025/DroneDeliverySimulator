09aaa9b25f0880ddcccdd9733559eeedf9d6d2c0
Daniel092025 <danbo092025@gmail.com>
2025-11-25 09:27:09 +0100

    First commit. Klargjort "struktur"

---
+++ Models/Models.cs
@@ -0,0 +1,18 @@
+﻿using System.Dynamic;
+using System.Runtime.InteropServices;
+
+namespace Models;
+
+public class DroneModel
+{
+    public string Name {get; set;}
+    public int MaxCheckPoints {get; set;}
+    public int DelayMs {get; set;}
+
+    public DroneModel (string name, int maxCheckPoints, int delayMs)
+    {
+        Name = name;
+        MaxCheckPoints = maxCheckPoints;
+        DelayMs = delayMs;
+    }
+}
---
+++ Simulations/ThreadRace.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace Simulations;
+
+public class ThreadRace 
+{
+    public static void Run()
+    {
+        Console.WriteLine("=== Thread Race === \n");
+
+        var drone1 = new DroneModel("Alpha", 5, 500);
+    }
+}
